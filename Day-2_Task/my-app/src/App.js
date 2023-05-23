1f25784aef45816837ae5849920f339465d89228
Shubham Joshi <shubhamjj1261@gmail.com>
2023-05-23 13:09:43 +0530

    commit improvements

--- Day-2_Task/my-app/src/App.js
+++ Day-2_Task/my-app/src/App.js
@@ -1,31 +1,66 @@
 // import logo from './logo.svg';
 import "./App.css";
-import { Route, Link, BrowserRouter, Routes } from "react-router-dom";
-
-import { Homepage } from "./Home";
-import { Apple } from "./Apple";
-
-import { NotFound } from "./notfound";
+import { Homepage } from "./HomePage";
+import { Apple } from "./apple";
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import { Notfound } from "./notfound";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./styles";
+// import { globalStyles } from './constants';
+// import Logo from "./logo.svg"
+// import siteLogo from "../public/logo192.png";
 
 const App = () => (
-  <BrowserRouter>
-    <div>
-      <Link to="/" style={{ marginLeft: 5 }}>
-        Home
-      </Link>
-      <Link to="/apple" style={{ marginLeft: 10 }}>
-        Apple
-      </Link>
-      <Link to="/applet" style={{ marginLeft: 15 }}>
-        Applet
-      </Link>
-    </div>
-    <Routes>
-      <Route path="/" element={<Homepage />}></Route>
-      <Route path="/apple" element={<Apple />}></Route>
-      <Route path="*" element={<NotFound />}></Route>
-    </Routes>
-  </BrowserRouter>
-);
+  <>
+    <ThemeProvider theme={theme}>
+      {/* <img src="localhost:3000/logo192.png" alt='Not found'></img> */}
+      {/* <img src={`${process.env.REACT_APP_HOSTED_URL}logo192.png`} alt='Not found'></img> */}
+      {/* <img src={Logo} alt='Not found'></img> */}
+      {/* <img src={siteLogo} alt='Not found'></img> */}
+      <BrowserRouter>
+        <div className="navbar-style">
+          {/* <div className="navbar-style"   style={{globalStyles.navbar,}}> */}
+          {/* <div   style={{
+      ...globalStyles.navbar,
+      }}
+      > */}
+          <Link
+            to="/"
+            style={{
+              marginLeft: 15,
+              padding: 5,
+            }}
+          >
+            Home
+          </Link>
+          <Link
+            to="/apple"
+            style={{
+              marginLeft: 15,
+              padding: 5,
+            }}
+          >
+            Apple
+          </Link>
+          <Link
+            to="/applet"
+            style={{
+              marginLeft: 15,
+              padding: 5,
+            }}
+          >
+            Applet
+          </Link>
 
-export default App;
+          {/* <Homepage></Homepage>
+      <Apple></Apple> */}
+        </div>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/apple" element={<Apple />}></Route>
+          <Route path="*" element={<Notfound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </>
+);
