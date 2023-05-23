7905065c3c4373b0febbf0f805683ce7f3a5410c
Shubham Joshi <shubhamjj1261@gmail.com>
2023-05-23 13:03:10 +0530

    commit day 3 task

---
+++ Day-2_Task/my-app/src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import "./App.css";
+import { Route, Link, BrowserRouter, Routes } from "react-router-dom";
+
+import { Homepage } from "./Home";
+import { Apple } from "./Apple";
+
+import { NotFound } from "./notfound";
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Link to="/" style={{ marginLeft: 5 }}>
+        Home
+      </Link>
+      <Link to="/apple" style={{ marginLeft: 10 }}>
+        Apple
+      </Link>
+      <Link to="/applet" style={{ marginLeft: 15 }}>
+        Applet
+      </Link>
+    </div>
+    <Routes>
+      <Route path="/" element={<Homepage />}></Route>
+      <Route path="/apple" element={<Apple />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
